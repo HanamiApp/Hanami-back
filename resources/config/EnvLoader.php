77766f7dd9a9deed18a6eac74efc7774662e0c35
Alessio Farinelli <alessio.farinelli.developer@gmail.com>
2020-10-11 21:54:35 +0200

    added a base EnvLoader

---
+++ resources/config/EnvLoader.php
@@ -0,0 +1,35 @@
+<?php
+
+
+class EnvLoader
+{
+   // Class variables
+   private $pathToEnvFile;
+   private $MY_ENV = Array();
+   
+   // costruttore
+   public function __construct($path)
+   {
+      $this->pathToEnvFile = $path;
+      $this->loadEnvVariables();
+   }
+
+   // methods
+   private function loadEnvVariables()
+   {
+      $envFile = file($this->pathToEnvFile . '/.env');
+      foreach($envFile as $variable) {
+         $splitted = explode('=', $variable);
+         $this->MY_ENV[$splitted[0]] = $splitted[1];
+      }
+   }
+
+   public function getVariable($key)
+   {
+      return $this->MY_ENV[$key];
+   }
+
+}
+
+
+?>
