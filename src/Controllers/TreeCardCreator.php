ecc66b2e3921d293479a9e5528276b4e045c580e
Alessio Farinelli <alessio.farinelli.developer@gmail.com>
2020-10-20 15:50:44 +0200

    feat(Controllers): added related User to every updateList item

--- src/Controllers/TreeCardCreator.php
+++ src/Controllers/TreeCardCreator.php
@@ -4,8 +4,10 @@
 
   namespace App\Controllers;
   use App\Data\Dao\UpdateDao as UpdateDao;
+  use App\Data\Dao\UtenteDao as UtenteDao;
   use App\Services\HTTP as HTTP;
   require_once __DIR__ . '/../Data/Dao/UpdateDao.php';
+  require_once __DIR__ . '/../Data/Dao/UtenteDao.php';
   require_once __DIR__ . '/../Services/HTTP.php';
 
 
@@ -17,9 +19,16 @@
 
       // prendiamo gli 'aggiornamenti' fatti all'albero con l'id fornito
       $UpdateDao = new UpdateDao();
+      $UtenteDao = new UtenteDao();
       // lista di aggiornamenti
       $updateList = $UpdateDao->getUpdatesByTreeId($id_tree);
-      HTTP::sendJsonResponse(202, 'messaggio');
+      
+
+      $u = [];
+      foreach($updateList as $up) {
+        array_push($u, [$up->__toString(), $UtenteDao->getById($up->getIdUtente())->__toString()]);
+      }
+      HTTP::sendJsonResponse(202, $u);
 
     }
 
