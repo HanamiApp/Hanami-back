ef88694cbf46cc2d7563b977f4059c2ce0cb9276
shewraar <simone@Air-di-Simone.homenet.telecomitalia.it>
2020-11-04 17:03:00 +0100

    GET&POST tree clear code

--- src/Controllers/PiantaController.php
+++ src/Controllers/PiantaController.php
@@ -31,10 +31,11 @@
     $post_json = json_decode(file_get_contents('php://input'));
     $PiantaDao = new PiantaDao();
     //check campi inseriti dall'utente che crea la pianta
-    /*if($post_json->{"genere"} == null || $post_json->{"specie"} == null)
+    
+    if($post_json->{"genere"} == null || $post_json->{"specie"} == null)
       echo "WARNING: missing_values_for_genere_or_specie";
     if($post_json->{"nome"} == null || $post_json->{"co2"} == null || $post_json->{"descrizione"} == null)
-      echo "ERROR: missing_values_for_nome_or_co2_or_descrizione";*/
+      echo "ERROR: missing_values_for_nome_or_co2_or_descrizione";
     // plant creation
     $Pianta = new Pianta($post_json->{"genere"}, $post_json->{"specie"}, $post_json->{"nome"}, $post_json->{"co2"}, $post_json->{"descrizione"});
     $PiantaDao->store($Pianta);
--- src/Controllers/QRCodeController.php
+++ src/Controllers/QRCodeController.php
@@ -12,11 +12,9 @@ class QRCodeController{
 
       $data = "localhost:8080/pianta/{$Pianta->getId()}";
       //$data = urlencode($data);
-
-      file_put_contents('/Users/simone/Desktop/Hanami/Hanami-back/src/Debug/url.txt', print_r($data, true), FILE_APPEND);
       
       $url = QRCodeController::API_URL."?cht=".QRCodeController::CHT."&chs=".$width."x".$height."&chl=".$data;
-      file_put_contents('/Users/simone/Desktop/Hanami/Hanami-back/src/Debug/qrcode.txt', print_r($url, true), FILE_APPEND);
+      
       if($encoding)
       {
          $url.="&choe".$encoding;
