6ac50d355cd507f31c8cfebf78eabe8f3e090918
Alessio Farinelli <alessio.farinelli.developer@gmail.com>
2020-11-21 17:07:26 +0100

    fixed type

--- src/Controllers/PiantaController.php
+++ src/Controllers/PiantaController.php
@@ -14,7 +14,7 @@
 
   class PiantaController{
 
-  //metodo per la get
+  //metodo per la GET
   public static function get($id)
   {
     $PiantaDao = new PiantaDao();
@@ -24,7 +24,7 @@
     echo json_encode($StatoPianta->__toString());
   }
 
-  //metodo per la PUT
+  //metodo per la POST
   public static function create()
   {
       
@@ -32,10 +32,11 @@
     $PiantaDao = new PiantaDao();
     //check campi inseriti dall'utente che crea la pianta
     
-    if($post_json->{"genere"} == null || $post_json->{"specie"} == null)
-      echo "WARNING: missing_values_for_genere_or_specie";
-    if($post_json->{"nome"} == null || $post_json->{"co2"} == null || $post_json->{"descrizione"} == null)
-      echo "ERROR: missing_values_for_nome_or_co2_or_descrizione";
+    if($post_json->{"genere"} === null || $post_json->{"specie"} === null)
+      die("WARNING: missing_values_for_genere_or_specie");
+    if($post_json->{"nome"} === null || $post_json->{"co2"} === null || $post_json->{"descrizione"} === null)
+      die("ERROR: missing_values_for_nome_or_co2_or_descrizione");
+
     // plant creation
     $Pianta = new Pianta($post_json->{"genere"}, $post_json->{"specie"}, $post_json->{"nome"}, $post_json->{"co2"}, $post_json->{"descrizione"});
     $PiantaDao->store($Pianta);
