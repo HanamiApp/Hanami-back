64980ac8810a9847a6ed5a12ece04bc0c5e763e5
Alessio Farinelli <alessio.farinelli.developer@gmail.com>
2020-11-22 09:59:36 +0100

    fix(Database): refactoring database

--- db/hanami.sql
+++ db/hanami.sql
@@ -1,238 +1,246 @@
 CREATE DATABASE IF NOT EXISTS hanami;
 USE hanami;
 
-DROP TABLE IF EXISTS gruppo_servizio;
-DROP TABLE IF EXISTS servizio;
-DROP TABLE IF EXISTS utente_gruppo;
-DROP TABLE IF EXISTS gruppo;
-DROP TABLE IF EXISTS utente_task;
-DROP TABLE IF EXISTS task;
-DROP TABLE IF EXISTS utente_obiettivo;
-DROP TABLE IF EXISTS obiettivo;
-DROP TABLE IF EXISTS aggiornamento;
-DROP TABLE IF EXISTS pianta;
-DROP TABLE IF EXISTS stato_pianta;
-DROP TABLE IF EXISTS stato_regalo;
-DROP TABLE IF EXISTS luogo;
-DROP TABLE IF EXISTS specie;
-DROP TABLE IF EXISTS spostamento;
-DROP TABLE IF EXISTS mezzo;
-DROP TABLE IF EXISTS notifica;
-DROP TABLE IF EXISTS utente;
+DROP TABLE IF EXISTS `group_service`;
+DROP TABLE IF EXISTS `service`;
+DROP TABLE IF EXISTS `user_group`;
+DROP TABLE IF EXISTS `group`;
+DROP TABLE IF EXISTS `user_task`;
+DROP TABLE IF EXISTS `task`;
+DROP TABLE IF EXISTS `user_goal`;
+DROP TABLE IF EXISTS `goal`;
+DROP TABLE IF EXISTS `update`;
+DROP TABLE IF EXISTS `plant`;
+DROP TABLE IF EXISTS `plant_state`;
+DROP TABLE IF EXISTS `gift_state`;
+DROP TABLE IF EXISTS `place`;
+DROP TABLE IF EXISTS `species`;
+DROP TABLE IF EXISTS `genus`;
+DROP TABLE IF EXISTS `trip`;
+DROP TABLE IF EXISTS `vehicle`;
+DROP TABLE IF EXISTS `notice`;
+DROP TABLE IF EXISTS `refresh_token`;
+DROP TABLE IF EXISTS `user`;
 
 
 
-CREATE TABLE utente( 
-   id int auto_increment primary key,
-   nome varchar(20) not null,
-   cognome varchar(30) not null,
-   email varchar(100) not null unique,
+CREATE TABLE `user`( 
+   `id` int auto_increment primary key,
+   `first_name` varchar(20) not null,
+   `last_name` varchar(30) not null,
+   `email` varchar(100) not null unique,
    `password` varchar(30) not null,
-   regione ENUM('abruzzo', 'basilicata', 'calabria', 'campania', 'emilia romagna', 
-               'friuli-venezia giulia', 'lazio', 'liguria', 'lombardia', 'marche', 'molise',
-               'piemonte', 'puglia', 'sardegna', 'sicilia', 'toscana', 'trentino-alto adige',
-               'umbria', 'valle d aosta', 'veneto') not null
-);
-CREATE TABLE refresh_tokens(
-   id_user int primary key,
-   token varchar(255) not null,
-   foreign key (id_user) references utente(id)
-      ON UPDATE CASCADE
-      ON DELETE CASCADE
-);
-/* inserimento utente di default per i test */
-INSERT INTO utente(nome, cognome, email, `password`, regione) VALUES('admin', 'admin', 'admin@admin.com','admin', 'ABRUZZO');
-
-CREATE TABLE notifica(
-   id int auto_increment primary key,
-   titolo varchar(30) not null,
-   testo text not null,
-   id_utente int,
-   foreign key (id_utente) references utente(id)
-      ON UPDATE CASCADE
-      ON DELETE CASCADE
+   `region` ENUM('ABRUZZO', 'BASILICATA', 'CALABRIA', 'CAMPANIA', 'EMILIA_ROMAGNA', 
+               'FRIULI_VENEZIA_GIULIA', 'LAZIO', 'LIGURIA', 'LOMBARDIA', 'MARCHE', 'MOLISE',
+               'PIEMONTE', 'PUGLIA', 'SARDEGNA', 'SICILIA', 'TOSCANA', 'TRENTINO_ALTO_ADIGE',
+               'UMBRIA', "VALLE_D_AOSTA", 'VENETO') not null
 );
 
-CREATE TABLE mezzo(
-   id int auto_increment primary key,
-   nome varchar(25) not null,
-   co2 float not null
+CREATE TABLE `refresh_token`(
+   `id_user` int primary key,
+   `token` varchar(255) not null,
+   foreign key (`id_user`) references `user`(`id`)
+      ON UPDATE cascade
+      ON DELETE cascade
+);
+/* inserimento `user` di default per i test */
+INSERT INTO `user`(`first_name`, `last_name`, `email`, `password`, `region`) VALUES('admin', 'admin', 'admin@admin.com','admin', 'ABRUZZO');
+
+CREATE TABLE `notice`(
+   `id` int auto_increment primary key,
+   `title` varchar(30) not null,
+   `text` text not null,
+   `id_user` int,
+   foreign key (`id_user`) references `user`(`id`)
+      ON UPDATE cascade
+      ON DELETE cascade
 );
 
-CREATE TABLE spostamento(
-   id int auto_increment primary key,
-   tempo int not null, 
-   lunghezza int not null, 
-   co2 float not null,
-   id_utente int,
-   id_mezzo int,
-   foreign key(id_utente) references utente(id)
-      ON UPDATE CASCADE
-      ON DELETE CASCADE,
-   foreign key(id_mezzo) references mezzo(id)
-      ON UPDATE CASCADE
-      ON DELETE CASCADE
+CREATE TABLE `vehicle`(
+   `id` int auto_increment primary key,
+   `name` varchar(25) not null,
+   `co2` float not null
 );
 
-CREATE TABLE specie(
-   id int auto_increment primary key,
-   genere varchar(50),
-   nome varchar(50),
-   co2 float not null,
-   descrizione text not null
+CREATE TABLE `trip`(
+   `id` int auto_increment primary key,
+   `time` int not null, 
+   `length` int not null, 
+   `co2` float not null,
+   `id_user` int,
+   `id_vehicle` int,
+   foreign key(`id_user`) references `user`(`id`)
+      ON UPDATE cascade
+      ON DELETE cascade,
+   foreign key(`id_vehicle`) references `vehicle`(`id`)
+      ON UPDATE cascade
+      ON DELETE cascade
 );
 
-CREATE TABLE luogo(
-   id int auto_increment primary key,
-   nome varchar(30) not null,
-   citta varchar(30) not null,
-   regione ENUM('abruzzo', 'basilicata', 'calabria', 'campania', 'emilia romagna', 
-               'friuli-venezia giulia', 'lazio', 'liguria', 'lombardia', 'marche', 'molise',
-               'piemonte', 'puglia', 'sardegna', 'sicilia', 'toscana', 'trentino-alto adige',
-               'umbria', "valle d'aosta", 'veneto') not null,
-   coordinate float
+CREATE TABLE `genus`(
+   `id` int auto_increment primary key,
+   `name` varchar(50)
 );
 
-CREATE TABLE stato_regalo(
-   id int auto_increment primary key,
-   stato ENUM('spedito', 'ricevuto', 'completato', 'in completamento') not null
+CREATE TABLE `species`(
+   `id` int auto_increment primary key,
+   `name` varchar(50),
+   `co2` float not null,
+   `description` text not null,
+   `id_genus` int,
+   foreign key (`id_genus`) references `genus`(`id`)
+      ON UPDATE cascade
+      ON DELETE cascade
 );
 
-CREATE TABLE stato_pianta(
-   id int auto_increment primary key,
-   stato ENUM('trasporto', 'piantata', 'deceduta') not null,
-   stato_vitale ENUM('buono stato', 'assetata', 'rinsecchita', 'incolta') not null,
-   giorno date,
-   id_pianta int,
-   foreign key (id_pianta) references pianta(id)
-      on delete cascade
-      on update cascade
+CREATE TABLE `place`(
+   `id` int auto_increment primary key,
+   `name` varchar(30) not null,
+   `city` varchar(30) not null,
+   `region` ENUM('ABRUZZO', 'BASILICATA', 'CALABRIA', 'CAMPANIA', 'EMILIA_ROMAGNA', 
+               'FRIULI_VENEZIA_GIULIA', 'LAZIO', 'LIGURIA', 'LOMBARDIA', 'MARCHE', 'MOLISE',
+               'PIEMONTE', 'PUGLIA', 'SARDEGNA', 'SICILIA', 'TOSCANA', 'TRENTINO_ALTO_ADIGE',
+               'UMBRIA', "VALLE_D_AOSTA", 'VENETO') not null,
+   `coordinate_x` float not null,
+   `coordinate_y` float not null
 );
 
-CREATE TABLE pianta(
-   id int auto_increment primary key,
-   nome varchar(50) not null,
-   regalo boolean default 0,
-   id_luogo int,
-   id_stato_pianta int,
-   id_stato_regalo int,
-   id_utente int,
-   id_specie int,
-   qrcode text
-   foreign key (id_luogo) references luogo(id)
-      on delete cascade
-      on update cascade,
-   foreign key (id_stato_pianta) references stato_pianta(id)
-      on delete cascade
-      on update cascade,
-   foreign key (id_stato_regalo) references stato_regalo(id)
-      on delete cascade
-      on update cascade,
-   foreign key (id_utente) references utente(id)
-      on delete cascade
-      on update cascade,
-   foreign key (id_specie) references specie(id)
-      on delete cascade
-      on update cascade
+CREATE TABLE `gift_state`(
+   `id` int auto_increment primary key,
+   `state` ENUM('IN_PROCESS', 'SHIPPED', 'RECEIVED') not null
+);
+
+CREATE TABLE `plant_state`(
+   `id` int auto_increment primary key,
+   `state` ENUM('IN_PROCESS', 'TRAVELIN', 'PLANTED', 'DIED') not null,
+   `condition` ENUM('HEALTHY', 'THIRSTY', 'DRY', 'UNCUT') not null,  -- in salute, assetata, secca, non tagliata
+   `day` date
+);
+
+CREATE TABLE `plant`(
+   `id` int auto_increment primary key,
+   `name` varchar(50) not null,
+   `gift` boolean default 0,
+   `id_place` int,
+   `id_plant_state` int,
+   `id_gift_state` int,
+   `id_user` int,
+   `id_species` int,
+   `qrcode` text,
+   foreign key (`id_place`) references `place`(`id`)
+      ON DELETE cascade
+      ON UPDATE cascade,
+   foreign key (`id_plant_state`) references `plant_state`(`id`)
+      ON DELETE cascade
+      ON UPDATE cascade,
+   foreign key (`id_gift_state`) references `gift_state`(`id`)
+      ON DELETE cascade
+      ON UPDATE cascade,
+   foreign key (`id_user`) references `user`(`id`)
+      ON DELETE cascade
+      ON UPDATE cascade,
+   foreign key (`id_species`) references `species`(`id`)
+      ON DELETE cascade
+      ON UPDATE cascade
 );
 
 /* inserimento di una pianta di prova */
-INSERT INTO pianta(nome) VALUES('baobab');
+INSERT INTO `plant`(`name`) VALUES('baobab');
 
-CREATE TABLE aggiornamento(
-   id int auto_increment primary key,
-   `data` date not null,
-   ora time,
-   intervento varchar(100) not null,
-   path_img varchar(1000),
-   id_pianta int,
-   id_utente int,
-   foreign key (id_pianta) references pianta(id)
-      on update cascade
-      on delete cascade,
-   foreign key (id_utente) references utente(id)
-      on update cascade
-      on delete cascade
+CREATE TABLE `update`(
+   `id` int auto_increment primary key,
+   `date` date not null,
+   `hour` time,
+   `operation` varchar(100) not null,
+   `path_img` varchar(1000),
+   `id_plant` int,
+   `id_user` int,
+   foreign key (`id_plant`) references `plant`(`id`)
+      ON UPDATE cascade
+      ON DELETE cascade,
+   foreign key (`id_user`) references `user`(`id`)
+      ON UPDATE cascade
+      ON DELETE cascade
 );
 
 /* inserimento aggiornamento di prova */
-INSERT INTO aggiornamento(`data`, intervento, id_pianta, id_utente) VALUES('2020-01-01', 'potatura', 1, 1);
-INSERT INTO aggiornamento(`data`, intervento, id_pianta, id_utente) VALUES('2020-01-02', 'controllo', 1, 1);
+INSERT INTO `update`(`date`, `operation`, `id_plant`, `id_user`) VALUES('2020-01-01', 'potatura', 1, 1);
+INSERT INTO `update`(`date`, `operation`, `id_plant`, `id_user`) VALUES('2020-01-02', 'controllo', 1, 1);
 
-CREATE TABLE obiettivo(
-   id int auto_increment primary key,
-   nome varchar(30) not null
+CREATE TABLE `goal`(
+   `id` int auto_increment primary key,
+   `name` varchar(30) not null
 );
 
-CREATE TABLE utente_obiettivo(
-   id int auto_increment primary key,
-   data_inizio date not null,
-   id_obiettivo int,
-   id_utente int,
-   foreign key (id_obiettivo) references obiettivo(id)
-      on delete cascade
-      on update cascade,
-   foreign key (id_utente) references utente(id)
-      on delete cascade
-      on update cascade
+CREATE TABLE `user_goal`(
+   `id` int auto_increment primary key,
+   `start_date` date not null,
+   `id_goal` int,
+   `id_user` int,
+   foreign key (`id_goal`) references `goal`(`id`)
+      ON DELETE cascade
+      ON UPDATE cascade,
+   foreign key (`id_user`) references `user`(`id`)
+      ON DELETE cascade
+      ON UPDATE cascade
 );
 
-CREATE TABLE task(
-   id int auto_increment primary key,
-   nome varchar(50) not null, 
-   scadenza date not null,
-   data_inizio date not null,
-   periodo int not null
+CREATE TABLE `task`(
+   `id` int auto_increment primary key,
+   `name` varchar(50) not null, 
+   `deadline` date not null,
+   `duration` int not null
 );
 
-CREATE TABLE utente_task(
-   id int auto_increment primary key,
-   id_task int,
-   id_utente int,
-   foreign key (id_task) references task(id)
-      on delete cascade
-      on update cascade,
-   foreign key (id_utente) references utente(id)
-      on delete cascade
-      on update cascade
+CREATE TABLE `user_task`(
+   `id` int auto_increment primary key,
+   `start_date` date not null,
+   `id_task` int not null,
+   `id_user` int not null,
+   foreign key (`id_task`) references `task`(`id`)
+      ON DELETE cascade
+      ON UPDATE cascade,
+   foreign key (`id_user`) references `user`(`id`)
+      ON DELETE cascade
+      ON UPDATE cascade
 );
 
-CREATE TABLE gruppo(
-   id int auto_increment primary key,
-   nome varchar(30) not null
+CREATE TABLE `group`(
+   `id` int auto_increment primary key,
+   `name` varchar(30) not null
 );
 
 /* gruppi utenza di default */
-INSERT INTO gruppo(nome) VALUES('ospite');
-INSERT INTO gruppo(nome) VALUES('amministratore');
-INSERT INTO gruppo(nome) VALUES('giardiniere');
+INSERT INTO `group`(`name`) VALUES('GUEST');
+INSERT INTO `group`(`name`) VALUES('ADMIN');
+INSERT INTO `group`(`name`) VALUES('GARDENER');
 
-CREATE TABLE utente_gruppo(
-   id int auto_increment primary key,
-   id_gruppo int,
-   id_utente int,
-   foreign key (id_gruppo) references gruppo(id)
-      on delete cascade
-      on update cascade,
-   foreign key (id_utente) references utente(id)
-      on delete cascade
-      on update cascade
+CREATE TABLE `user_group`(
+   `id` int auto_increment primary key,
+   `id_group` int,
+   `id_user` int,
+   foreign key (`id_group`) references `group`(`id`)
+      ON DELETE cascade
+      ON UPDATE cascade,
+   foreign key (`id_user`) references `user`(`id`)
+      ON DELETE cascade
+      ON UPDATE cascade
 );
 
-CREATE TABLE servizio(
-   id int auto_increment primary key,
-   nome varchar(50) not null,
-   sconto int default 0
+CREATE TABLE `service`(
+   `id` int auto_increment primary key,
+   `name` varchar(50) not null,
+   `discount` int default 0
 );
 
-CREATE TABLE gruppo_servizio(
-   id int auto_increment primary key,
-   id_servizio int,
-   id_gruppo int,
-   foreign key (id_servizio) references servizio(id)
-      on delete cascade
-      on update cascade,
-   foreign key (id_gruppo) references gruppo(id)
-      on delete cascade
-      on update cascade
+CREATE TABLE `group_service`(
+   `id` int auto_increment primary key,
+   `id_service` int,
+   `id_group` int,
+   foreign key (`id_service`) references `service`(`id`)
+      ON DELETE cascade
+      ON UPDATE cascade,
+   foreign key (`id_group`) references `group`(`id`)
+      ON DELETE cascade
+      ON UPDATE cascade
 );
